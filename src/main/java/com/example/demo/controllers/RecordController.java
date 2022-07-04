f742f9a3a1903270a83c23fe377fd0b523423fc7
Arunkumar <arunrangerking@gmail.com>
2022-07-04 07:55:38 +0530

    CRUD - done

--- src/main/java/com/example/demo/controllers/RecordController.java
+++ src/main/java/com/example/demo/controllers/RecordController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +25,53 @@ public class RecordController {
         return "====" + name;
     }
 
+    /**
+     * add user method
+     * @param users
+     * @return Users
+     */
     @PostMapping("/add")
     public Users addUser(@RequestBody Users users){
         return userService.saveUser(users);
+    }
+
+    /**
+     * fin user by id method
+     * @param id
+     * @return Users
+     */
+    @GetMapping("/find/{id}")
+    public Users findUser(@PathVariable int id){
+        return userService.findUser(id);
+    }
+
+    /**
+     * fin user by name method
+     * @param name
+     * @return Users
+     */
+    @GetMapping("/find/name/{name}")
+    public Users findUserByName(@PathVariable String name){
+        return userService.findUserByName(name);
+    }
+
+    /**
+     * get users list method
+     * @return array
+     */
+    @GetMapping("/list")
+    public List<Users> userList(){
+        return userService.getUserList();
+    }
+
+    /**
+     * delete user by id
+     * @param id
+     * @return string
+     */
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id){
+        return this.userService.deleteUser(id);
     }
 
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,49 @@ public class UserService {
     @Autowired
     private UsersRepository usersRepository;
 
+    /**
+     * Save user data to db
+     * @param users
+     * @return Users
+     */
     public Users saveUser(Users users){
         return this.usersRepository.save(users);
+    }
+
+    /**
+     * find user by id
+     * @param id
+     * @return user/null
+     */
+    public Users findUser(int id){
+        return this.usersRepository.findById(id).orElse(null);
+    }
+
+     /**
+     * find user by name
+     * @param id
+     * @return user/null
+     */
+    public Users findUserByName(String name){
+        return this.usersRepository.findByName(name);
+    }
+
+    /**
+     * get all users list
+     * @return array
+     */
+    public List<Users> getUserList(){
+        return this.usersRepository.findAll();
+    }
+
+    /**
+     * delete by id
+     * @param id
+     * @return array
+     */
+    public String deleteUser(int id){
+        this.usersRepository.deleteById(id);
+        return "User deleted " + id;
     }
 
 }
